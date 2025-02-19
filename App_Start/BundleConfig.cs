5c844ac2e24e1f5ef3cdf108519b1a00a7208702
George <mr.creciunel@list.ru>
2025-02-19 19:05:53 +0200

    New Project

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -26,6 +26,8 @@ namespace CryptoWallet.App_Start
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            //BundleTable.EnableOptimizations = true;
         }
     }
 }
--- Global.asax.cs
+++ Global.asax.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
-namespace CryptoWallet
+namespace CryptoWallet.App_Start
 {
     public class Global : HttpApplication
     {
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-           AreaRegistration.RegisterAllAreas();
-           RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+            
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
