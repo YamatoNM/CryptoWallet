007b6a44bc1376d19a19075d233ddb366fd47f73
George <mr.creciunel@list.ru>
2025-02-28 16:01:29 +0200

    Adding Buy/Sell pages Fixing errors

---
+++ Controllers/SellCryptoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryptoWallet.Controllers
+{
+    public class SellCryptoController : Controller
+    {
+        // GET: SellCrypto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
